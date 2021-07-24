f84400a30388754ff04184ad282ba0583cd9a259
Nurlykhan <nurlykhan.salamatuly@samgau.com>
2021-07-25 01:40:29 +0600

    feat: type Title

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -2,9 +2,11 @@ import React from "react";
 
 type TitleProps = {
   label: string;
+  size?: 12 | 16 | 24 | 32 | 48;
 };
 
 export const Title: React.FC<TitleProps> = (props) => {
-  const { label } = props;
-  return <h1>{label}</h1>;
+  const { label, size = 24 }: TitleProps = props;
+  
+  return <p style={{ fontSize: size }}>{label}</p>;
 };
