89096d4ca78f67aa2d1510943a4cdf49d11f4e65
nurlykhan <nurlykhan.salamatuly@samgau.com>
2021-09-27 04:32:24 +0600

    refactor:

--- src/components/CButton/Button.spec.js
+++ src/components/CButton/Button.spec.js
@@ -4,27 +4,20 @@ import { CButton } from "./index";
 const setUp = (props) => shallow(<CButton {...props} />);
 
 describe("Button", () => {
-  let component;
-  beforeEach(() => {
-    component = setUp();
-  });
-
   it("should render component", () => {
+    const component = setUp();
     expect(component).toMatchSnapshot();
   });
 
   it("should call onClick method", () => {
     const mockCallback = jest.fn();
-    const Button = setUp({ onClick: mockCallback });
+    const component = setUp({ onClick: mockCallback });
 
     expect(mockCallback.mock.calls.length).toBe(0);
 
-    Button.find("button").simulate("click");
-    Button.find("button").simulate("click");
+    component.find("button").simulate("click");
+    component.find("button").simulate("click");
 
     expect(mockCallback.mock.calls.length).toBe(2);
-
-    
-
   });
 });
